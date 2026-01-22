015e8cf0ccf0322b8570739c61148e9590638d08
AndrewHuff6 <77467984+AndrewHuff6@users.noreply.github.com>
2026-01-22 15:53:42 -0600

    Add LinkedList and Node structure in list_queue.cpp
    Implement basic structure for a singly linked list.

--- Misc./list_queue.cpp
+++ Misc./list_queue.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+// one pointer implies a singly linked list
+// two pointers implies a doubly linked list
+// N pointers implies graphs
+struct Node {
+	// data here
+	int data;
+	Node* next;
+
+	Node(int data) : data(data), next(nullptr) {}
+};
+
+// singly linked list
+class LinkedList {
+	Node* head;
+	Node* tail;
+public:
+	LinkedList() : head(nullptr), tail(nullptr) {}
+};
+
+int main(int argc, char** argv) {
+	LinkedList L;
+	return 0;
+}
